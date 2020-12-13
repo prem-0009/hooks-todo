1a0c9c3206df0d3f05b67bf39ff1a5a3ad1d0641
prem <prem.sherpa@codeimmersives.com>
2020-12-12 21:33:42 -0500

    strike-through

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
-
-// import './App.css';
-
 import Todos from "./components/Todos";
 import { useState } from "react";
 
-
-
 function App() {
-
-  const [todos, setTodos]= useState([]);
   return (
     <div className="App">
-      <Todos 
+      <Todos />
       
-      // onSubmit={(text)=>setTodos([{text, complete:false}, ...todos])} 
-      />
-      <div>
-        {/* {
-          todos.map(({text})=>
-            <div key={text}>{text}</div>
-            )
-          } */}
-      </div>
     </div>
   );
 }
